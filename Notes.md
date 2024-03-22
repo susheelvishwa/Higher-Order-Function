e232790752282c787d5de1941067ebb7e02c39c8
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-23 02:25:17 +0530

    HOF

--- Notes.md
+++ Notes.md
@@ -1,3 +1,22 @@
 # All about Hight order function
 
 understand --> ".this"
+
+
+few tecnical termonology for interview porpose
+
+1. function statment
+2. function expression
+3. anonymous function
+4. callback function
+5. HOF(higher order function)
+
+some inbuilt HOF's
+1. Map
+2. ForEach
+3. Filter
+4. Reduce
+
+are for arrays only
+
+
---
+++ task2/script.js
@@ -0,0 +1,28 @@
+// print the user name of all the object ussing foreach
+
+let jsondata = [
+  {
+    Name: "susheel",
+    cource: "B.tech",
+    passing_year: 2024,
+  },
+  {
+    Name: "pillu",
+    cource: "B.tech",
+    passing_year: 2024,
+  },
+  {
+    Name: "sonu",
+    cource: "B.tech",
+    passing_year: 2024,
+  },
+  {
+    Name: "batman",
+    cource: "B.tech",
+    passing_year: 2024,
+  },
+];
+
+jsondata.forEach(function (ele) {
+    console.log("user name: ",ele.Name);
+})
