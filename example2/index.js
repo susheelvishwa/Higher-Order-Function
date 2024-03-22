a7fccbbd803082417ccd2cd80e646b146f6a7bb0
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-23 01:03:10 +0530

    understand diffenrent types of function such as

---
+++ example2/index.js
@@ -0,0 +1,59 @@
+// few tecnical termonology for interview porpose
+
+
+// ----------------------------------------------
+// 1. function statment /named function
+
+// function sayhii() {
+//     return 'hello'
+// }
+// console.log(sayhii());
+// ----------------------------------------------
+
+// 2. function expression
+// #function are first class citizen
+// #function can be assign as value
+
+// let sum = function (a, b) {
+//     return a+b
+// }
+// console.log(sum(5,6));
+
+// ----------------------------------------------
+
+// 3. anonymous function
+// function without name is called  anonymous function
+
+// function() {
+    
+// }
+
+// --------------------------------------------------------------------------------------
+
+// 4. callback function:- function which are passed as Parameters inside another function
+
+// function parent(program) {
+//     return program()
+// }
+// function greet() {
+//     return "hello"
+// }
+// console.log(parent(greet));
+
+// ----------------------------------------------
+
+// 5.higher order function (HOF)
+
+// function which are passed as Parameters inside another function this function called as a higher order function
+
+// --------------------------------------
+// function parent(program) {
+//     return program()                     ------------->>>> here this function is called as a higher ordr function
+// }
+// ------------------------------------------
+// function greet() {
+//     return "hello"
+// }
+// console.log(parent(greet));
+
+// ----------------------------------------------
