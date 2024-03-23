59c83e2a750d5fa2fbb66ce6c9cfc3d02109ed75
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-03-23 12:58:10 +0530

    update

--- task2/script.js
+++ task2/script.js
@@ -1,28 +1,31 @@
 // print the user name of all the object ussing foreach
 
-let jsondata = [
-  {
-    Name: "susheel",
-    cource: "B.tech",
-    passing_year: 2024,
-  },
-  {
-    Name: "pillu",
-    cource: "B.tech",
-    passing_year: 2024,
-  },
-  {
-    Name: "sonu",
-    cource: "B.tech",
-    passing_year: 2024,
-  },
-  {
-    Name: "batman",
-    cource: "B.tech",
-    passing_year: 2024,
-  },
-];
+// let jsondata = [
+//   {
+//     Name: "susheel",
+//     cource: "B.tech",
+//     passing_year: 2024,
+//   },
+//   {
+//     Name: "pillu",
+//     cource: "B.tech",
+//     passing_year: 2024,
+//   },
+//   {
+//     Name: "sonu",
+//     cource: "B.tech",
+//     passing_year: 2024,
+//   },
+//   {
+//     Name: "batman",
+//     cource: "B.tech",
+//     passing_year: 2024,
+//   },
+// ];
 
-jsondata.forEach(function (ele) {
-    console.log("user name: ",ele.Name);
-})
+// jsondata.forEach(function (ele) {
+//     console.log("user name: ",ele.Name);
+// })
+
+
+
